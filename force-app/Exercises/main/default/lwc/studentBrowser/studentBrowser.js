1b4416b2fd9a1ac646696a8178fe383f36652047
rcaputto <rcaputto@dex602.com>
2024-09-12 17:30:14 +0000

    Exercise 5-1 Completed

--- force-app/Exercises/main/default/lwc/studentBrowser/studentBrowser.js
+++ force-app/Exercises/main/default/lwc/studentBrowser/studentBrowser.js
@@ -1,31 +1,65 @@
 import { LightningElement, wire } from 'lwc';
+import getStudents from '@salesforce/apex/StudentBrowser.getStudents';
 import { publish, MessageContext } from 'lightning/messageService';
 import SELECTED_STUDENT_CHANNEL from '@salesforce/messageChannel/SelectedStudentChannel__c';
-import getStudents from '@salesforce/apex/StudentBrowser.getStudents';
+import { NavigationMixin } from 'lightning/navigation';
 
-export default class StudentBrowser extends LightningElement {
-	selectedDeliveryId = '';
-	selectedInstructorId = '';
-
+export default class StudentBrowser extends NavigationMixin(LightningElement) {
 	@wire(getStudents, { instructorId: '$selectedInstructorId', courseDeliveryId: '$selectedDeliveryId' })
 	students;
 
+	cols = [
+		{
+			fieldName:"Name", 
+			label: "Name"
+		},
+		{
+			fieldName:"Title", 
+			label: "Title",
+			hiddenOnMobile: true
+		},
+		{
+			fieldName:"Phone", 
+			label: "Phone",
+			type: "phone"
+		},
+		{
+			fieldName:"Email", 
+			label: "E-Mail",
+			type: "email"
+		}
+	];
+	
+
 	@wire(MessageContext) messageContext;
 
-	handleStudentSelected(event){
-		const studentId = event.detail.studentId;
-		this.updateSelectedStudent(studentId);
-		}
-
-	updateSelectedStudent(studentId){
-			publish(this.messageContext, SELECTED_STUDENT_CHANNEL, {
-			studentId: studentId
-			});
-			}
+	selectedDeliveryId = '';
+	selectedInstructorId = '';
 
 	handleFilterChange(event){
 		this.selectedDeliveryId = event.detail.deliveryId;
 		this.selectedInstructorId = event.detail.instructorId;
 	}
+
+	handleStudentSelected(event){
+		const studentId = event.detail.studentId;
+		this.updateSelectedStudent(studentId);
+	}
+
+	updateSelectedStudent(studentId){
+		publish(this.messageContext, SELECTED_STUDENT_CHANNEL, { studentId: studentId });
+	}
+
+	handleRowDblClick(event) {
+		const studentId = event.detail.pk;
+		this[NavigationMixin.Navigate]({
+		type: 'standard__recordPage',
+		attributes: {
+			recordId: studentId,
+			objectApiName: 'Contact',
+			actionName: 'edit'
+			}
+			});
+		}
 
 }
